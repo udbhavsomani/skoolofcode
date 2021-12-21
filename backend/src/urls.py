2c1ec70bb1511637632abaf4609d16e063bac238
Udbhav18 <udbhavsomani18@gmail.com>
2021-12-21 07:10:18 +0530

    heroku branch

--- backend/src/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from rest_framework.authtoken.views import obtain_auth_token
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    path('auth/', obtain_auth_token),
-]
